4dafc296dc28493ec52cf597b0e05ef7ba0ea5ae
OhMinecraftLauncher <3514158849@qq.com>
2023-09-30 17:46:59 +0800

    WinDivert=>WinDivert(Sharp) 目前仍无法修改包，继续尝试中……

--- WLANPackageGet/Program.cs
+++ WLANPackageGet/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics.Eventing.Reader;
-using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
-using PacketDotNet;
-using SharpPcap;
-using SharpPcap.LibPcap;
-using SharpPcap.WinPcap;
+using System.Threading;
+using System.Threading.Tasks;
+using WinDivertSharp;
 
 namespace WLANPackageGet
 {
@@ -14,115 +13,46 @@ namespace WLANPackageGet
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < CaptureDeviceList.Instance.Count; i++)
+            IntPtr handle = WinDivert.WinDivertOpen("outbound and tcp.PayloadLength > 0 and tcp.PayloadLength < 50", WinDivertLayer.Network, 0, WinDivertOpenFlags.None);
+
+            int err = Marshal.GetLastWin32Error();
+            if (err != 0 || handle == IntPtr.Zero)
             {
-                string[] sps = CaptureDeviceList.Instance[i].ToString().Split('\n');
-                for (int j = 0; j < sps.Length; j++)
-                {
-                    if (sps[j].Contains("FriendlyName: "))
-                    {
-                        Console.WriteLine($"[{i}] " + sps[j].Replace("FriendlyName: ", ""));
-                    }
-                }
+                Console.WriteLine("无法打开 WinDivert 句柄：" + err);
+                return;
             }
-            string con = Console.ReadLine();
-            int num = int.Parse(con);
 
-            ICaptureDevice device = CaptureDeviceList.Instance[num];
-
-            device.OnPacketArrival += Device_OnPacketArrival;
-
-            device.Open(DeviceMode.Promiscuous);
-
-            device.StartCapture();
-
-            while (true) ;
-        }
-
-        private static void Device_OnPacketArrival(object sender, CaptureEventArgs e)
-        {
-            RawCapture capture = e.Packet;
-            Packet packet = Packet.ParsePacket(capture.LinkLayerType, capture.Data);
-            IpPacket ippacket = IpPacket.GetEncapsulated(packet);
-            if (ippacket != null)
+            WinDivertBuffer buffer = new WinDivertBuffer();
+            WinDivertAddress address = new WinDivertAddress();
+            uint readLen = 0;
+            while (true)
             {
-                TcpPacket tcpPacket = TcpPacket.GetEncapsulated(packet);
-                UdpPacket udpPacket = UdpPacket.GetEncapsulated(packet);
-                if (tcpPacket != null)
+                NativeOverlapped native = new NativeOverlapped();
+                WinDivert.WinDivertRecvEx(handle, buffer, 0, ref address, ref readLen, ref native);
+                WinDivertParseResult result = WinDivert.WinDivertHelperParsePacket(buffer, readLen);
+                unsafe
                 {
-                    Console.WriteLine("协议：Tcp");
-                    Console.WriteLine("源地址：" + ippacket.SourceAddress.ToString() + " [" + tcpPacket.SourcePort.ToString() + ']');
-                    Console.WriteLine("目标地址：" + ippacket.DestinationAddress.ToString() + " [" + tcpPacket.DestinationPort.ToString() + ']');
-                }
-                else if (udpPacket != null)
-                {
-                    Console.WriteLine("协议：Udp");
-                    Console.WriteLine("源地址：" + ippacket.SourceAddress.ToString() + " [" + udpPacket.SourcePort.ToString() + ']');
-                    Console.WriteLine("目标地址：" + ippacket.DestinationAddress.ToString() + " [" + udpPacket.DestinationPort.ToString() + ']');
-                }
-                else
-                {
-                    Console.WriteLine("协议：Ip");
-                    Console.WriteLine("源地址：" + ippacket.SourceAddress.ToString());
-                    Console.WriteLine("目标地址：" + ippacket.DestinationAddress.ToString());
-                }
-                byte[] bytes = ippacket.BytesHighPerformance.Bytes;
-                Console.Write("报文信息：");
-                for (int i = 0;i < bytes.Length;i++)
-                {
-                    Console.Write((bytes[i] >= 33 && bytes[i] <= 126) ? Encoding.ASCII.GetString(new byte[1] { bytes[i] }) : ".");
-                }
-                Console.WriteLine();
-                bool flag_s = false;
-                bool flag_d = false;
-                ReadOnlyCollection<PcapAddress> ans = ((WinPcapDevice)e.Device).Addresses;
-                foreach (PcapAddress an in ans)
-                {
-                    if (an != null && an.Addr != null && an.Addr.ipAddress != null)
+                    byte[] bytes = new byte[result.PacketPayloadLength];
+                    if (result.PacketPayload != null)
                     {
-                        if (ippacket.SourceAddress.ToString() == an.Addr.ipAddress.ToString())
+                        Marshal.Copy((IntPtr)result.PacketPayload, bytes, 0, (int)result.PacketPayloadLength);
+                        for (int i = 0; i < result.PacketPayloadLength; i++)
                         {
-                            flag_s = true;
-                        }
-                        if (ippacket.DestinationAddress.ToString() == an.Addr.ipAddress.ToString())
-                        {
-                            flag_d = true;
+                            Console.Write((bytes[i] >= 33 && bytes[i] <= 126) ? Encoding.ASCII.GetString(new byte[1] { bytes[i] }) : ".");
                         }
                     }
                 }
-                if (flag_s)
+                /*for (int i = 0; i < readLen; i++)
                 {
-                    Console.WriteLine("方向：发出");
+                    Console.Write((buffer[i] >= 33 && buffer[i] <= 126) ? Encoding.ASCII.GetString(new byte[1] { buffer[i] }) : ".");
                 }
-                else if (flag_d)
-                {
-                    Console.WriteLine("方向：收入");
-                }
-                else
-                {
-                    Console.WriteLine("方向：未知");
-                }
-            }
-            Console.WriteLine();
-        }
-
-        /*private static void Program_OnPacketArrival(object sender, PacketCapture e)
-        {
-            try
-            {
-                RawCapture capture = e.GetPacket();
-                Packet packet = Packet.ParsePacket(capture.LinkLayerType, capture.Data);
-                TcpPacket tcp = (TcpPacket)packet;
-                IpPacket iPPacket = (IpPacket)packet;
-                Console.WriteLine("源地址：" + iPPacket.SourceAddress);
-                Console.WriteLine("目标地址" + iPPacket.DestinationAddress);
+                Console.WriteLine();*/
                 Console.WriteLine();
-                //Console.WriteLine(Encoding.Default.GetString(packet.BytesSegment.ActualBytes()));
+                if (!WinDivert.WinDivertSendEx(handle, buffer, readLen, 0, ref address)) 
+                {
+                    Console.WriteLine("Send Error:" + Marshal.GetLastWin32Error());
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-        }*/
+        }
     }
 }
